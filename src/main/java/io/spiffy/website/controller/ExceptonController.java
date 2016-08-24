55d7adbbf980223929c5c7fd8ccdcbaf5236ce24
John <john@spiffy.io>
2016-08-24 14:56:09 -0700

    [SPIFFY-96] Catch uris that get linked incorrectly

--- src/main/java/io/spiffy/website/controller/ExceptonController.java
+++ src/main/java/io/spiffy/website/controller/ExceptonController.java
@@ -1,5 +1,9 @@
 package io.spiffy.website.controller;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +21,13 @@ public class ExceptonController extends Controller {
     private static final String CODE_KEY = "code";
     private static final String MESSAGE_KEY = "message";
     private static final String STACKTRACE_KEY = "stacktrace";
+    private static final Set<Character> OOPSIE_ENDERS;
+
+    static {
+        final Set<Character> oopsieEnders = new HashSet<>();
+        ".!?>)]};:'\"_-+*".chars().mapToObj(c -> (char) c).forEach(c -> oopsieEnders.add(c));
+        OOPSIE_ENDERS = Collections.unmodifiableSet(oopsieEnders);
+    }
 
     @ExceptionHandler(NoHandlerFoundException.class)
     public ModelAndView noHandlerFoundException(final Context context, final NoHandlerFoundException e) {
@@ -33,6 +44,20 @@ public class ExceptonController extends Controller {
         context.addAttribute(CODE_KEY, status + ": " + status.getReasonPhrase());
 
         if (HttpStatus.NOT_FOUND.equals(status)) {
+            final String uri = context.getRequestUri();
+            final char ender = uri.charAt(uri.length() - 1);
+            if (OOPSIE_ENDERS.contains(ender)) {
+                final StringBuilder builder = new StringBuilder();
+                StringBuilder window = new StringBuilder();
+                for (final char c : uri.toCharArray()) {
+                    window.append(c);
+                    if (!OOPSIE_ENDERS.contains(c)) {
+                        builder.append(window);
+                        window = new StringBuilder();
+                    }
+                }
+                return redirect(builder.toString(), context);
+            }
             context.addAttribute(MESSAGE_KEY, "this is not the page you were looking for.");
         } else {
             context.addAttribute(MESSAGE_KEY, "something borked.");
